12250ebb35aebb47098670799f9301ed49a270b6
fretwang <wangyuanbo68@gmail.com>
2025-08-20 22:35:57 +0800

    Updated google.ts

--- src/config/google.ts
+++ src/config/google.ts
@@ -9,7 +9,7 @@ export const GOOGLE_CONFIG = {
 };
 
 // Google OAuth URLs
-export const GOOGLE_OAUTH_URL = 'https://accounts.google.com/oauth2/v2/auth';
+export const GOOGLE_OAUTH_URL = 'https://accounts.google.com/o/oauth2/v2/auth';
 export const GOOGLE_TOKEN_URL = 'https://oauth2.googleapis.com/token';
 export const GOOGLE_USERINFO_URL = 'https://www.googleapis.com/oauth2/v2/userinfo';
 
