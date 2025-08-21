53710d27b5916fee77c92ba9d31f2da5d688e829
fretwang <wangyuanbo68@gmail.com>
2025-08-22 02:01:09 +0800

    修复logger未定义错误

--- src/contexts/CreditContext.tsx
+++ src/contexts/CreditContext.tsx
@@ -1,6 +1,7 @@
 import React, { createContext, useContext, useState, ReactNode, useEffect } from 'react';
 import { useAuth } from './AuthContext';
 import apiService from '../services/apiService';
+import { logger } from '../utils/logger';
 
 interface Transaction {
   id: string;
